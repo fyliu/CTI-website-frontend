96fd81921e47cfc6bb2d7e2b04c03783fdd3695b
Bhaggya Bala <bhaggya@gmail.com>
2021-08-04 22:08:07 -0700

    Sort Parent&Child Alphabetically (#749)
    * Sort Parent&Child Alphabetically
    
    * Used Prettier and modified code as per review/
    
    * Minor cleanup for ContributorThumbnail.js (#727)
    
    * Made changes as per review
    
    * Changed Files as per review
    
    * Fix linting errors (#727)
    
    Co-authored-by: bruceplai <bruceplai@yahoo.com>

--- src/pages/Contributors/Affiliated.js
+++ src/pages/Contributors/Affiliated.js
@@ -1,29 +1,27 @@
-import React,{ useState } from "react";
-import Typography from "@material-ui/core/Typography";
-import { DropdownArrow } from "../../components/DropdownArrow.js";
+import React, { useState } from 'react';
+import Typography from '@material-ui/core/Typography';
+import { DropdownArrow } from '../../components/DropdownArrow.js';
 import Grid from '@material-ui/core/Grid';
-import { AffiliatedOrganizations } from "./AffiliatedOrganizations";
+import { AffiliatedOrganizations } from './AffiliatedOrganizations';
 import clsx from 'clsx';
 import Link from '@material-ui/core/Link';
-import { makeStyles } from '@material-ui/core/styles'
-
-
+import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
   titleStyle: {
     color: theme.palette.secondary.dark,
     textAlign: 'center',
   },
-  gpGrid:{
+  gpGrid: {
     display: 'flex',
-    flexDirection:'row',
+    flexDirection: 'row',
     alignItems: 'center',
     margin: 'auto',
     backgroundColor: theme.palette.background.default,
     borderRadius: '6px',
-    padding:'8px 16px',
+    padding: '8px 16px',
     '& h4': {
-      paddingLeft:'24px',
+      paddingLeft: '24px',
       color: theme.palette.secondary.dark,
     },
     '& a:link': {
@@ -36,7 +34,7 @@ const useStyles = makeStyles((theme) => ({
       height: '64px',
     },
   },
-  blueColor:{
+  blueColor: {
     backgroundColor: theme.palette.secondary.dark,
     '& h4': {
       color: theme.palette.text.secondary,
@@ -48,14 +46,14 @@ const useStyles = makeStyles((theme) => ({
       color: theme.palette.text.secondary,
     },
   },
-  flexGrid:{
+  flexGrid: {
     flexGrow: 1,
     justifyContent: 'flex-end',
   },
-  dropDownGrid:{
+  dropDownGrid: {
     margin: 'auto',
     backgroundColor: theme.palette.background.default,
-    padding:'24px 0px',
+    padding: '24px 0px',
   },
   contributorIcon: {
     marginTop: '24%',
@@ -72,55 +70,104 @@ const useStyles = makeStyles((theme) => ({
 }));
 /* eslint complexity: [0, 0]*/
 export const Affiliated = (props) => {
-  const { organizations, inputValue, classes, organizationData, affiliatedSepOpen,  searchCount, affiliatedCount, totalaffiliatedCount, setAfflnSepOpen, checkboxValue } = props
-  const classesLocal = useStyles()
+  const {
+    organizations,
+    inputValue,
+    classes,
+    organizationData,
+    affiliatedSepOpen,
+    searchCount,
+    affiliatedCount,
+    totalaffiliatedCount,
+    setAfflnSepOpen,
+    checkboxValue,
+  } = props;
+  const classesLocal = useStyles();
   const [gp, setGp] = useState(false);
   const handleClickGrid = () => {
-    setGp(!gp)
+    setGp(!gp);
     setAfflnSepOpen(true);
   };
 
   return (
-    <Grid >
-      <Grid style={{ padding:'40px' }}>
+    <Grid>
+      <Grid style={{ padding: '40px' }}>
         <Typography variant='h4' className={classesLocal.titleStyle}>
           Affiliated Organizations
-          { searchCount ? `(${affiliatedCount}/${totalaffiliatedCount})`  : `(${totalaffiliatedCount})`   }
+          {searchCount
+            ? `(${affiliatedCount}/${totalaffiliatedCount})`
+            : `(${totalaffiliatedCount})`}
         </Typography>
       </Grid>
-      <Grid item xs={12} sm={10} className={clsx(classesLocal.gpGrid, {
-        [classesLocal.blueColor]: gp === true,
-      })} >
+      <Grid
+        item
+        xs={12}
+        sm={10}
+        className={clsx(classesLocal.gpGrid, {
+          [classesLocal.blueColor]: gp === true,
+        })}
+      >
         <Grid>
-          <img src="/images/code_for_All.png" alt="code for all logo" />
+          <img src='/images/code_for_All.png' alt='code for all logo' />
         </Grid>
         <Grid>
           <Typography variant='h4' noWrap>
-            <Link href="/organization/code-for-all"  target="_blank"  rel="noreferrer noopener">Code for All</Link>
-            { searchCount ? `(${affiliatedCount}/${totalaffiliatedCount})`  : ` (${totalaffiliatedCount})` }
+            <Link
+              href='https://codeforall.org'
+              target='_blank'
+              rel='noreferrer noopener'
+            >
+              Code for All
+            </Link>
+            {searchCount
+              ? `(${affiliatedCount}/${totalaffiliatedCount})`
+              : ` (${totalaffiliatedCount})`}
           </Typography>
         </Grid>
         <Grid>
-          { checkboxValue  ?
+          {checkboxValue ? (
             <Typography>
-              <img className={classesLocal.contributorIcon} src='/images/Gparent_contributed.svg' alt="contributor-icon" />
+              <img
+                className={classesLocal.contributorIcon}
+                src='/images/Gparent_contributed.svg'
+                alt='contributor-icon'
+              />
             </Typography>
-            : " "
-          }
+          ) : (
+            ' '
+          )}
         </Grid>
-        <Grid item container className={classesLocal.flexGrid} onClick={handleClickGrid}>
+        <Grid
+          item
+          container
+          className={classesLocal.flexGrid}
+          onClick={handleClickGrid}
+        >
           <DropdownArrow open={gp} handleArrow={handleClickGrid} />
         </Grid>
       </Grid>
-      <Grid >
-        {affiliatedSepOpen && gp && (
-          !organizations['Code for All'] ? !inputValue ? <h3 className={classes.loaders}>Loading...</h3> : <h3 className={classes.loaders}>No Results</h3>
-            :<Grid item xs={12} sm={10} className={classesLocal.dropDownGrid}>
-              <AffiliatedOrganizations organizations={organizations} inputValue={inputValue} data={organizationData} checkboxValue={checkboxValue}  />
+      <Grid>
+        {affiliatedSepOpen &&
+          gp &&
+          (!organizations['Code for All'] ? (
+            !inputValue ? (
+              <h3 className={classes.loaders}>Loading...</h3>
+            ) : (
+              <h3 className={classes.loaders}>No Results</h3>
+            )
+          ) : (
+            <Grid item xs={12} sm={10} className={classesLocal.dropDownGrid}>
+              <AffiliatedOrganizations
+                organizations={organizations}
+                inputValue={inputValue}
+                organizationData={organizationData}
+                checkboxValue={checkboxValue}
+              />
             </Grid>
-        )}
+          ))}
       </Grid>
     </Grid>
-  )};
+  );
+};
 
 export default Affiliated;
