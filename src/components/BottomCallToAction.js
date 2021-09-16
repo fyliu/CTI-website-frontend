d8c8bd1aa3770db5be2b2c3bb72fdcb9cae22a08
nrrao <nandanarao@yahoo.com>
2021-09-16 17:28:29 -0400

    added new gif

--- src/components/BottomCallToAction.js
+++ src/components/BottomCallToAction.js
@@ -4,13 +4,28 @@ import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import { useTheme } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
+const useStyles = makeStyles((theme) => ({
+  typoHeading: {
+    textAlign: 'center',
+    [theme.breakpoints.down('sm')]: {
+      fontSize: '1.75rem',
+    },
+  },
+}));
 
 const defaultBackgroundColor = 'transparent'; // see https://developer.mozilla.org/en-US/docs/Web/CSS/background-color - 'transparent' is initial value
 const defaultButtonHref = 'mailto:civictechindex@hackforla.org';
 const defaultButtonText = 'Contact Us';
 
-const BottomCallToAction = ({ color, buttonHref = defaultButtonHref, buttonText = defaultButtonText, heading }) => {
+const BottomCallToAction = ({
+  color,
+  buttonHref = defaultButtonHref,
+  buttonText = defaultButtonText,
+  heading,
+}) => {
+  const classes = useStyles();
   const theme = useTheme();
   let bgColor = defaultBackgroundColor;
   if (color === 'primary') {
@@ -22,9 +37,17 @@ const BottomCallToAction = ({ color, buttonHref = defaultButtonHref, buttonText 
 
   return (
     <Box style={boxStyle} py={8} mx={-3}>
-      <Grid container direction='column' justify='center' alignItems='center' spacing={4}>
+      <Grid
+        container
+        direction='column'
+        justify='center'
+        alignItems='center'
+        spacing={4}
+      >
         <Grid item>
-          <Typography variant='h2'>{heading}</Typography>
+          <Typography variant='h3' className={classes.typoHeading}>
+            {heading}
+          </Typography>
         </Grid>
         <Grid item>
           <Button href={buttonHref}>{buttonText}</Button>
